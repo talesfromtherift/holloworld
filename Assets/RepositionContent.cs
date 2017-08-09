47e4ddb0bcc382d02ce31dd81126d7f09c5fd1a7
Peter Koch <peterept@gmail.com>
2017-08-09 22:08:58 +1000

    Added reposition content script

---
+++ Assets/RepositionContent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepositionContent : MonoBehaviour {
+
+    public Transform Anchor;
+
+    void OnApplicationFocus(bool focus) {
+        if (focus) {
+            StartCoroutine(RepositionContentCoroutine());
+        }
+    }
+
+    IEnumerator RepositionContentCoroutine() {
+        yield return new WaitForEndOfFrame();
+        transform.position = Anchor.position;
+    }
+}
