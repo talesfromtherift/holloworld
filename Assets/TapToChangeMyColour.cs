359e939e2d72a2a60eea17051fabb6ff8cf948d5
Peter Koch <peterept@gmail.com>
2017-08-19 08:58:27 +1000

    gaze input

---
+++ Assets/TapToChangeMyColour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class TapToChangeMyColour : MonoBehaviour, IInputClickHandler, IFocusable {
+
+   public void OnInputClicked(InputClickedEventData eventData) {
+        GetComponent<Renderer>().material.color = Random.ColorHSV();
+    }
+
+    public void OnFocusEnter() {
+        transform.localScale *= 1.2f;
+    }
+    public void OnFocusExit() {
+        transform.localScale /= 1.2f;
+    }
+}
