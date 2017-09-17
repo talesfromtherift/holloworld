7d8c0b74c3d544431339d2487120ddc563f79c87
Peter Koch <peterept@gmail.com>
2017-09-17 18:25:47 +1000

    added webcam capture

---
+++ Assets/HoloCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoloCamera : MonoBehaviour {
+
+    WebCamTexture webcam;
+
+	void Start () {
+        webcam = new WebCamTexture();
+        webcam.Play();
+        Debug.LogFormat("webcam: {0} {1} x {2}", webcam.deviceName, webcam.width, webcam.height);
+	}
+	
+    public Texture2D TakePhoto() {
+        Debug.Log("Take Photo");
+
+        Texture2D webcamImage = new Texture2D(webcam.width, webcam.height);
+        webcamImage.SetPixels(webcam.GetPixels());
+        webcamImage.Apply();
+
+        return webcamImage;
+    }
+
+    public void TakePhotoToPreview(Renderer preview) {
+        Texture2D image = TakePhoto();
+        preview.material.mainTexture = image;
+
+        // update the aspect ratio to match webcam
+        float aspectRatio = (float)image.width / (float)image.height;
+        Vector3 scale = preview.transform.localScale;
+        scale.x = scale.y * aspectRatio;
+        preview.transform.localScale = scale;
+    }
+}
---
+++ Assets/HoloTap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.VR.WSA.Input;
+
+public class HoloTap : MonoBehaviour {
+
+    [Tooltip("The event fired on a Holo tap.")]
+    public UnityEvent Tap;
+
+    GestureRecognizer recognizer;
+
+    void Awake() {
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        recognizer.StartCapturingGestures();
+    }
+
+    void OnEnable() {
+        recognizer.TappedEvent += Recognizer_TappedEvent;
+    }
+
+    void OnDisable() {
+        recognizer.TappedEvent -= Recognizer_TappedEvent;
+    }
+
+    void Update() {
+#if UNITY_EDITOR
+        // simulate tap with mouse button
+        if (Input.GetMouseButtonDown(0)) {
+            Recognizer_TappedEvent(
+                InteractionSourceKind.Other,
+                1,
+                Camera.main.ScreenPointToRay(Input.mousePosition));
+        }
+#endif
+    }
+
+    private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay) {
+        Tap.Invoke();
+    }
+}
